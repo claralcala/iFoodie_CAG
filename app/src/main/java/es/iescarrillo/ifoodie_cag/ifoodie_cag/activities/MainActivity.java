6dc1be5c066bc9ac6ea5ad8863f652563126283f
Clara Alcalá García <calcgar312@iescarrillo.es>
2024-02-05 13:04:35 +0100

    cambios_menu

--- app/src/main/java/es/iescarrillo/ifoodie_cag/ifoodie_cag/activities/MainActivity.java
+++ app/src/main/java/es/iescarrillo/ifoodie_cag/ifoodie_cag/activities/MainActivity.java
@@ -1,16 +1,44 @@
 package es.iescarrillo.ifoodie_cag.ifoodie_cag.activities;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+
 
 import es.iescarrillo.ifoodie_cag.R;
 
 public class MainActivity extends AppCompatActivity {
 
+    private DrawerLayout drawerLayout;
+
+    private Toolbar toolbar;
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        loadComponents();
+        //metodo para indicar que el toolbar hara la funcion de barra de acciones
+        setSupportActionBar(toolbar);
+
+        ActionBarDrawerToggle actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.open_menu, R.string.close_menu);
+
+        //Listener para capturar los eventos que se produzcan en el menu lateral
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+
+        //Evento para sincronizar el estado del boton hamburguesa con el estado del menu lateral
+
+        actionBarDrawerToggle.syncState();
+    }
+
+    private void loadComponents(){
+        drawerLayout = findViewById(R.id.drawer_layout);
+        toolbar=findViewById(R.id.toolbar);
     }
 }
