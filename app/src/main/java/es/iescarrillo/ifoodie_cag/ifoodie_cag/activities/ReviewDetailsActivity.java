1330737ca307470ca2bcddd933c6ca4dd23aef36
Clara Alcalá García <calcgar312@iescarrillo.es>
2024-02-05 17:50:59 +0100

    c

---
+++ app/src/main/java/es/iescarrillo/ifoodie_cag/ifoodie_cag/activities/ReviewDetailsActivity.java
@@ -0,0 +1,19 @@
+package es.iescarrillo.ifoodie_cag.ifoodie_cag.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import es.iescarrillo.ifoodie_cag.R;
+
+public class ReviewDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_review_details);
+
+
+
+    }
+}
